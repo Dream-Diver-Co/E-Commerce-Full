4541c2f3c07f7b4510f878aa85edeff120b6ffba
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-07-08 09:01:24 +0600

    08

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -14,11 +14,11 @@
 
                 <a href="{{ route('admin.dashboard') }}" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('admin/assets/images/Dream.png') }}" alt="logo-sm-light" height="22">
+                        <img class="admin-logo-sm" src="{{ asset('admin/assets/images/Dream.png') }}" alt="logo-sm-light" height="22">
                     </span>
                     <span class="logo-lg">
 
-                        <img src="{{ asset('admin/assets/images/Dream1.png') }}" alt="logo-light" style="width: 240px; height:66px;" height="20">
+                        <img class="admin-logo" src="{{ asset('admin/assets/images/Dream1.png') }}" alt="logo-light" height="20">
                     </span>
                 </a>
             </div>
