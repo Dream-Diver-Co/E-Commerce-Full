2008418211c772eedaf6746f39c3206feff70727
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-07-11 16:24:15 +0600

    update

--- resources/views/frontend/page/men_panjabi.blade.php
+++ resources/views/frontend/page/men_panjabi.blade.php
@@ -26,17 +26,17 @@
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('storage/'. $item->image) }}" alt="">
+                        <img class="img-fluid w-100" style="height: 300px; width: 300px;" src="{{ asset('storage/'. $item->image) }}" alt="">
                         <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="{{ $item->name }}"  data-price="{{ $item->price }}" data-image="{{ asset('storage/'. $item->image) }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            {{-- <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi " data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi1.jpg') }}" href="#"><i class="far fa-heart"></i></a> --}}
-                            {{-- <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 1" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi1.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a> --}}
+                            <a href="" title="View Panjabi" class="btn btn-outline-dark btn-square view-btn" data-name="{{ $item->name }}"  data-price="{{ $item->price }}" data-image="{{ asset('storage/'. $item->image) }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
+                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="{{ $item->name }}" data-price="{{ $item->price }}" data-image="{{ asset('storage/'. $item->image) }}" href="#"><i class="far fa-heart"></i></a>
+                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="{{ $item->name }} " data-price="{{ $item->price }}" data-image="{{ asset('storage/'. $item->image) }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
                         </div>
                     </div>
                     <div class="text-center py-4">
                         <a class="h6 text-decoration-none text-truncate" href="">{{ $item->name }}</a>
                         <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>{{ $item->price }}</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
+                            <h5>${{ $item->price }}</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mb-1">
                             <small class="fa fa-star text-primary mr-1"></small>
@@ -50,188 +50,6 @@
                 </div>
             </div>
             @endforeach
-            {{-- <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('frontend/img/men/panjabi/Panjabi2.jpg') }}" alt="">
-                        <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="Panjabi 2"  data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi2.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi 2" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi2.jpg') }}" href="#"><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 2" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi2.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Panjabi 2</a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star-half-alt text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('frontend/img/men/panjabi/Panjabi3.jpeg') }}" alt="">
-                        <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="Panjabi 3"  data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi3.jpeg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi 3" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi3.jpeg') }}" href="#"><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 3" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi3.jpeg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Panjabi 3</a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star-half-alt text-primary mr-1"></small>
-                            <small class="far fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('frontend/img/men/panjabi/Panjabi4.jpg') }}" alt="">
-                        <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="Panjabi 4"  data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi4.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi 4" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi4.jpg') }}" href="#"><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 4" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi4.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Panjabi 4</a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="far fa-star text-primary mr-1"></small>
-                            <small class="far fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('frontend/img/men/panjabi/Panjabi5.jpg') }}" alt="">
-                        <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="Panjabi 5"  data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi5.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi 5" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi5.jpg') }}" href="#"><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 5" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi5.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Panjabi 5</a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('frontend/img/men/panjabi/Panjabi6.jpg') }}" alt="">
-                        <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="Panjabi 6"  data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi6.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi 6" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi6.jpg') }}" href="#"><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 6" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi6.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Panjabi 6</a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star-half-alt text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('frontend/img/men/panjabi/Panjabi7.jpg') }}" alt="">
-                        <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="Panjabi 7"  data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi7.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi 7" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi7.jpg') }}" href="#"><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 7" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi7.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Panjabi 7</a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star-half-alt text-primary mr-1"></small>
-                            <small class="far fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-                <div class="product-item bg-light mb-4">
-                    <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" style="height: 300px; width: 300px;"  style="height: 300px; width: 300px;"   src="{{ asset('frontend/img/men/panjabi/Panjabi8.jpg') }}" alt="">
-                        <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square view-btn" data-name="Panjabi 8"  data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi8.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-outline-dark btn-square product-heart-btn" data-name="Panjabi 8" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi8.jpg') }}" href="#"><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square add-btn add-to-cart-btn" data-name="Panjabi 8" data-price="459" data-image="{{ asset('frontend/img/men/panjabi/Panjabi8.jpg') }}" data-miles="35,000 mi" data-transmission="Auto" data-hp="700 hp" ><i class="fa fa-shopping-cart"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Panjabi 8</a>
-                        <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$123.00</h5><h6 class="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center mb-1">
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="far fa-star text-primary mr-1"></small>
-                            <small class="far fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
         </div>
     </div>
     <!-- Products End -->
